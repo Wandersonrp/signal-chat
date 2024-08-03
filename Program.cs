516c4342b3b85644cecc8fec4f5a2b3052d1d3ab
Wandersonrp <wandersonrp.49@hotmail.com>
2024-08-03 18:02:39 -0300

    feat: add ui

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using SignalChat.Components;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "SignalChatCookie";
+        options.LoginPath = "/auth/google-login";
+    })
+    .AddGoogle(options =>
+    {
+        var googleAuthSection = builder.Configuration.GetSection(key: "Authentication:Google");
+        options.ClientId = googleAuthSection[key: "ClientId"] ?? throw new ArgumentNullException("Google Authentication Client Id is null");
+        options.ClientSecret = googleAuthSection[key: "ClientSecret"] ?? throw new ArgumentNullException("Google Authentication Client Secret is null");
+
+        options.Scope.Add("Profile");
+        options.Events.OnCreatingTicket = context =>
+        {
+            var pictureUri = context.User.GetProperty("picture").GetString();
+#pragma warning disable CS8604 // Possível argumento de referência nula.
+#pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
+            context.Identity.AddClaim(new Claim("picture", pictureUri));
+#pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
+#pragma warning restore CS8604 // Possível argumento de referência nula.
+            return Task.CompletedTask;
+        };
+    });
 
 var app = builder.Build();
 
@@ -21,7 +55,17 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapControllers();
+});
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+  
