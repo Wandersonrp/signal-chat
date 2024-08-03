8cf3e63cd4189edb1fc1cb11eb9c02d81c92e9b9
Wandersonrp <wandersonrp.49@hotmail.com>
2024-08-03 15:57:37 -0300

    feat: ui

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    mode: "jit",
+    content: ["./**/*.razor", "./wwwroot/index.html"],
+    theme: {
+        extend: {
+            colors: {
+                slack_bg: "#2B092A",
+                slack_nav: "#3E103F"
+            }
+        }
+    },
+    plugins: []
+}; 
